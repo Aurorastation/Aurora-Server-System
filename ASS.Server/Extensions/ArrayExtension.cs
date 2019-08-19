568c45ad8725411202296f7826a178334bf2b111
Karolis2011 <Karolis2011@users.noreply.github.com>
2019-08-20 00:27:58 +0300

    PreAppend extension optimization.

--- ASS.Server/Extensions/ArrayExtension.cs
+++ ASS.Server/Extensions/ArrayExtension.cs
@@ -8,6 +8,10 @@ namespace ASS.Server.Extensions
     {
         public static T[] PreAppend<T>(this T[] array, params T[] toAppend)
         {
+            if (array.Length == 0)
+                return toAppend;
+            if (toAppend.Length == 0)
+                return array;
             var n = new T[toAppend.Length + array.Length];
             toAppend.CopyTo(n, 0);
             array.CopyTo(n, toAppend.Length);
